c80d0f4817276c9aad736d8cdeecaa8462e81599
unknown <bd7q9887u@code.edu.az>
2023-10-11 00:38:28 +0400

    CustomMethods

---
+++ CustMeth/Program.cs
@@ -0,0 +1,22 @@
+﻿//Contains(),Replace(),Substring(),Trim() methodlarını custom şəkildə yazmaq
+namespace CustMeth
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            String @string = new String();
+            @string.name = "    Javidan    ";
+/*            Console.WriteLine(@string.ContainMethod('I'));
+
+            Console.WriteLine(@string.ReplaceMethod('a', 'i'));
+
+            Console.WriteLine(@string.SubStringMethod(2));*/
+
+            Console.WriteLine(@string.TrimMethod());
+
+
+        }
+
+    }
+}
---
+++ CustMeth/String.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Text;
+
+namespace CustMeth
+{
+    internal class String
+    {
+        public string name;
+
+        public bool ContainMethod(char x)
+        {
+            for (int i = 0; i < name.Length; i++)
+            {
+            if (x == name[i])
+                {
+                    return true;
+                }
+
+            }
+            return false;
+        }
+        public string ReplaceMethod(char oldChar , char newChar)
+        {
+            StringBuilder newString = new StringBuilder();
+            for(int i=0; i < name.Length; i++) 
+            { 
+
+                if (oldChar == name[i])
+                {
+                    newString.Append(newChar);
+                }
+                else
+                {
+                    newString.Append(name[i]);
+                }
+            }
+            return newString.ToString();
+        }
+
+        public string SubStringMethod(int start)
+        {
+            StringBuilder newString = new StringBuilder();
+            
+            for (int i = start; i < name.Length; i++)
+            {
+                newString.Append(name[i]);
+            }
+            return newString.ToString();
+        }
+
+        public string TrimMethod()
+        {
+            StringBuilder newString = new StringBuilder();
+            int startIndex = 0;
+            int endIndex=name.Length-1;
+
+            for (int i = 0; i < name.Length; i++)
+            {
+                if (name[i]!=' ')
+                {
+                    startIndex = i;
+                    break;
+                }
+            }
+            for (int i = name.Length-1; i >= 0; i--)
+            {
+                if (name[i]!=' ')
+                {
+                    endIndex = i;
+                    break;
+                }
+            }
+            for (int i = startIndex; i <=endIndex; i++)
+            {
+                newString.Append(name[i]);
+            }
+            return newString.ToString();
+
+
+
+        }
+
+
+
+        }
+
+}
