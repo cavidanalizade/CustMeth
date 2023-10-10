085a7fecec7731d1934b6ad3ebd621ab6f9f5f91
unknown <bd7q9887u@code.edu.az>
2023-10-11 01:13:56 +0400

    PersonTask

---
+++ PersonTask/Person.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+
+namespace PersonTask
+{
+    internal class Person
+    {
+
+
+        private string fullName;
+        public string FullName
+        {
+            get { return fullName; }
+            set
+            {
+                if (IsValidFullName(value))
+                {
+                    fullName = value;
+                }
+                else
+                {
+                    Console.WriteLine("FulName minimum 2 sozden ibaret olmalidir ve boyuk herfle baslamalidir."); 
+                }
+            }
+        }
+
+        public int Age { get; set; }
+        public string PhoneNumber { get; set; }
+
+        private bool IsValidFullName(string fullName)
+        {
+            if (string.IsNullOrWhiteSpace(fullName))
+            {
+                return false;
+            }
+
+            string[] nameParts = fullName.Split(' ');
+            if (nameParts.Length < 2)
+            {
+                return false;
+            }
+
+            foreach (string namePart in nameParts)
+            {
+                if (string.IsNullOrWhiteSpace(namePart) || !char.IsUpper(namePart[0]))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public Person(string Fullname , string PhoneNumber , int Age)
+        {
+            this.FullName = Fullname;
+            this.Age = Age;
+            this.PhoneNumber = PhoneNumber;
+            
+        }
+    }
+
+}
+
---
+++ PersonTask/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PersonTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person=new Person("Javidan alizada","3848394" , 3 );
+        }
+    }
+}
